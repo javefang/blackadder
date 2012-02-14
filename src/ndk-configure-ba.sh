21158805117a353c5c041bce1af47b144617139a
Xinghong Fang <xf214@cam.ac.uk>
2012-02-14 20:46:11 +0000

    build script fixed, added CC variable

--- src/ndk-configure-ba.sh
+++ src/ndk-configure-ba.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-SYSROOT="$MY_NDK/sysroot"
-CC="arm-linux-androideabi-gcc"
-CXX="arm-linux-androideabi-g++"
-./configure \
+SYSROOT="$MY_NDK/sysroot" \
+CC="arm-linux-androideabi-gcc" \
+CXX="arm-linux-androideabi-g++" \
+./configure --host=arm-linux-androideabi\
 --with-click=/data/click \
 --prefix=/data/click \
 CXXFLAGS="-fno-rtti -fno-exceptions" \
