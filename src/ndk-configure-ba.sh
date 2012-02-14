97fe6fae55fb61502cf6a3f40456421815826021
Xinghong Fang <xf214@cam.ac.uk>
2012-02-14 20:48:09 +0000

    missing space added in the build script

--- src/ndk-configure-ba.sh
+++ src/ndk-configure-ba.sh
@@ -2,7 +2,7 @@
 SYSROOT="$MY_NDK/sysroot" \
 CC="arm-linux-androideabi-gcc" \
 CXX="arm-linux-androideabi-g++" \
-./configure --host=arm-linux-androideabi\
+./configure --host=arm-linux-androideabi \
 --with-click=/data/click \
 --prefix=/data/click \
 CXXFLAGS="-fno-rtti -fno-exceptions" \
