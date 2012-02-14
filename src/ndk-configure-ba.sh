a7026b341837bc6d3a9c2ff69fc2610b24a44da9
Xinghong Fang <xf214@cam.ac.uk>
2012-02-14 20:28:37 +0000

    building script simplified.

--- src/ndk-configure-ba.sh
+++ src/ndk-configure-ba.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 SYSROOT="$MY_NDK/sysroot"
-LIBGCC=`arm-linux-androideabi-gcc -print-libgcc-file-name`
-./configure --host=arm-linux-androideabi \
+CC="arm-linux-androideabi-gcc"
+CXX="arm-linux-androideabi-g++"
+./configure \
 --with-click=/data/click \
 --prefix=/data/click \
-CPPFLAGS="-I$SYSROOT/usr/include" \
 CXXFLAGS="-fno-rtti -fno-exceptions" \
-LDFLAGS="-Wl,-rpath-link=$SYSROOT/usr/lib,-dynamic-linker=/system/bin/linker" \
-LIBS="-lm -lstdc++ -lc $LIBGCC" \
+
