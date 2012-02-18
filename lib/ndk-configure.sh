ec7c37c59b4982b41a97c193810aeb778aee419d
Xinghong Fang <xf214@cam.ac.uk>
2012-02-18 18:47:24 +0000

    files cleaned up. future works: 1. this library will be compiled by ndk-build in the future, no update will be made here

--- lib/ndk-configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SYSROOT="$MY_NDK/sysroot" \
-CC="arm-linux-androideabi-gcc" \
-CXX="arm-linux-androideabi-g++" \
-./configure --host=arm-linux-androideabi \
-CXXFLAGS="-fno-rtti -fno-exceptions"
